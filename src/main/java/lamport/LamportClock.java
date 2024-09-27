9a17c3405290c481595b871a79eef67a7a28ca90
H <geppscrossautoparts@gmail.com>
2024-09-27 18:05:00 +0930

    First commit

--- src/main/java/lamport/LamportClock.java
+++ src/main/java/lamport/LamportClock.java
@@ -9,23 +9,23 @@ public class LamportClock {
     private volatile AtomicInteger time; // for multi-threading access
 
     public LamportClock() {
-        this.time = new AtomicInteger(0);
+        this.time = new AtomicInteger(0); // begin at 0
     }
 
     // Process the tie break given a received timestamp from another entity
+    // when sent or a time is received, this counts as an event, so increment the time
     public int processEvent(int receivedTime) {
         // Accept the greater of the two times received
-        this.time = max(time, receivedTime);
-        return this.time;
+        return time.updateAndGet(current -> Math.max(current, receivedTime) + 1);
     }
 
-    void updateTime() {
-        this.time++;
+    // increment the time, and then return the current time
+    public int updateTime() {
+        return time.incrementAndGet(); // increase time by 1 unit
     }
 
     // Get current time of the Lamport Clock
     public int getTime() {
-        return this.time;
+        return time.get();
     }
-
 }
